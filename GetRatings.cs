a93eece9c971acb166c03486e4df18199dd8cf28
pkal42 <pritikalani@hotmail.com>
2021-11-17 09:15:42 +0000

    Updated get ratings

--- GetRatings.cs
+++ GetRatings.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace OpenHack
 {
@@ -14,22 +15,18 @@ namespace OpenHack
     {
         [FunctionName("GetRatings")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "ratings/{userId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "bfyoc",
+                collectionName: "ratings",
+                ConnectionStringSetting = "CosmosDBConnection",
+                SqlQuery = "select * from c where c.userId = {userId}"
+            )] IEnumerable<Rating> ratings,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            log.LogInformation("C# HTTP trigger function received a GetRatings request.");
+            
+            return new OkObjectResult(ratings);
         }
     }
 }
