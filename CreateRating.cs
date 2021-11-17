143182129f37c1a92a6780a6c5d7a1a13bd8091a
pkal42 <pritikalani@hotmail.com>
2021-11-17 10:41:13 +0000

    updated create -rating with user id and productid checks from Fabien.

--- CreateRating.cs
+++ CreateRating.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -12,6 +13,7 @@ namespace OpenHack
 {
     public static class CreateRating
     {
+        private static readonly HttpClient client = new HttpClient();
         [FunctionName("CreateRating")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
@@ -23,19 +25,47 @@ namespace OpenHack
         {
             log.LogInformation("C# HTTP trigger function received a create rating request.");
 
-            string reqBody = await new StreamReader(req.Body).ReadToEndAsync();
-            Rating rating = JsonConvert.DeserializeObject<Rating>(reqBody);
+            bool invalidRequest = false;
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Rating rating = JsonConvert.DeserializeObject<Rating>(requestBody);
+            string responseMessage = string.Empty;            
+            
+                       
+            HttpResponseMessage response = await client.GetAsync($"https://serverlessohapi.azurewebsites.net/api/GetUser?userId={rating.UserId}");
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                invalidRequest = true;
+                responseMessage = $"User '{rating.UserId}' is invalid.";
+            }
+            else
+            {
+                log.LogInformation($"User '{rating.UserId}' is valid.");
+                response = await client.GetAsync($"https://serverlessohapi.azurewebsites.net/api/GetProduct?productId={rating.ProductId}");
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    invalidRequest = true;
+                    responseMessage = $"Product '{rating.ProductId}' is invalid.";
+                }
+                else{     
+                    log.LogInformation($"Product '{rating.ProductId}' is valid.");                           
+                    if (rating.RatingScore < 0 || rating.RatingScore > 5)
+                    {
+                        invalidRequest = true;
+                        responseMessage = $"Rating '{rating}' value must be an integer between 0 and 5.";
+                    }
+                    rating.RatingId = Guid.NewGuid();
+                    rating.TimeStamp = DateTime.UtcNow;
 
-            if (rating.RatingScore >=0 && rating.RatingScore <=5) {
-               rating.RatingId = Guid.NewGuid();
-               rating.TimeStamp = DateTime.Now;
-            
-               await ratingsout.AddAsync(rating);
-            
+                    await ratingsout.AddAsync(rating);
+                }
+            }           
+            if (invalidRequest)
+            {
+                return new BadRequestObjectResult(responseMessage);
+            }
+            else {
                 return new CreatedResult("/rating", rating);
             }
-
-            return new BadRequestResult();
         }
     }
 }
